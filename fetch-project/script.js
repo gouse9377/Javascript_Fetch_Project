842d1a356ce842b68b9149d79b84e436f057027e
Shaik Gouse Mohiddin <gousemohiddin9377@gmail.com>
2024-12-19 16:19:47 +0530

    Fetch-Project

---
+++ fetch-project/script.js
@@ -0,0 +1,34 @@
+let postStart = 0;
+document.getElementById("loadpost").addEventListener("click",function(){
+    loadposts(postStart);
+
+});
+    
+function loadposts(username){
+    const url= 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch(url)
+    .then(respone=>{
+        if(!respone.ok){
+            throw new Error('post is not found: ${response.status}');
+        }
+        return respone.json();
+    })
+    .then(useData => {
+        const postsContainer = document.getElementById("posts");
+        useData.forEach(post => {
+            const postElement = document.createElement("div");
+            postElement.classList.add("posts");
+            postElement.innerHTML = `     
+             <div class="post-title">post #${post.id}: ${post.title} </div>
+            <p>${post.body}</p>
+            `;
+            postsContainer.appendChild(postElement); 
+        });
+        postStart +=10;
+    })
+    .catch(error =>{
+        console.error("Error fetching user:",error);
+    });
+    
+}
